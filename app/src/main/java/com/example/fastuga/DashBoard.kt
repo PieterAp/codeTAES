74d4ba6b7b1be1219936310cdbe0d00ab637aaba
igor-2191815 <2191815@my.ipleiria.pt>
2022-12-03 17:37:40 +0000

    PRJ-27 edit profile implementation and validation + statistics fragment

--- app/src/main/java/com/example/fastuga/DashBoard.kt
+++ app/src/main/java/com/example/fastuga/DashBoard.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
@@ -16,14 +17,21 @@ import com.android.volley.RequestQueue
 import com.android.volley.Response
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
+import com.google.android.material.navigation.NavigationView
 
 
 class DashBoard : AppCompatActivity() {
     private lateinit var requestQueue: RequestQueue
+    private lateinit var profileImageView: ImageView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dash_board)
+
+        val navigationView: NavigationView = findViewById(R.id.nav_view)
+        val header: View = navigationView.getHeaderView(0)
+        profileImageView = header.findViewById<View>(R.id.profileImageNav) as ImageView
 
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
@@ -41,6 +49,12 @@ class DashBoard : AppCompatActivity() {
         toggle.syncState()
 
         requestQueue = Volley.newRequestQueue(this)
+
+        profileImageView.setOnClickListener{
+            val intent = Intent(applicationContext, ProfileActivity::class.java)
+            startActivity(intent)
+            drawer.close()
+        }
 
         val logoutButton = findViewById<View>(R.id.nav_logout) as TextView
 
